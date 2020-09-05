e1d942a45949baadfa3cf7334de03390c25538a8
Aldeir Francisco <dide123francisco@gmail.com>
2020-09-05 19:24:33 -0300

    Busca de produtos por nome e categorias

--- src/main/java/com/aldeir/springProject/repositories/ProdutoRepository.java
+++ src/main/java/com/aldeir/springProject/repositories/ProdutoRepository.java
@@ -1,11 +1,21 @@
 package com.aldeir.springProject.repositories;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
+import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.aldeir.springProject.domain.Categoria;
 import com.aldeir.springProject.domain.Produto;
 
 @Repository
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 //objeto desse tipo e capaz de fazer operacoes de acesso a dados referentes a objeto Categoria
+	//@Query("SELECT DISTINCT obj FROM Produto obj INNER JOIN obj.categorias cat WHERE obj.nome LIKE %:nome% AND cat IN :categorias")
+	@Transactional(readOnly=true)
+	Page<Produto> findDistinctByNomeContainingAndCategoriasIn(@Param("nome") String nome, @Param("categorias") List<Categoria> categorias, Pageable pageRequest);
 }
---
+++ src/main/java/com/aldeir/springProject/services/ProdutoService.java
@@ -0,0 +1,38 @@
+package com.aldeir.springProject.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
+import org.springframework.stereotype.Service;
+
+import com.aldeir.springProject.domain.Categoria;
+import com.aldeir.springProject.domain.Pedido;
+import com.aldeir.springProject.domain.Produto;
+import com.aldeir.springProject.repositories.CategoriaRepository;
+import com.aldeir.springProject.repositories.ProdutoRepository;
+
+@Service
+public class ProdutoService {
+    
+	@Autowired
+	private ProdutoRepository repo;
+	@Autowired
+	private CategoriaRepository  categoriaRepository;
+	
+	public Produto busca(Integer id) {
+		Optional<Produto> obj = repo.findById(id);
+		
+		
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ",  Tipo: " + Pedido.class.getName()));
+	}
+	public Page<Produto> search(String nome, List<Integer> ids, Integer page, Integer linesPerPage, String orderBy, String direction) {
+		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction), orderBy);
+		List<Categoria> categorias = categoriaRepository.findAllById(ids);
+		return repo.findDistinctByNomeContainingAndCategoriasIn(nome, categorias, pageRequest);	
+	}
+}
